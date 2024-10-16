870f34d6d47148730fbcc3c2447fed3ab1d769a6
M4ch4d0C0l1d4r <103066475+M4ch4d0C0l1d4r@users.noreply.github.com>
2024-10-16 12:11:46 -0400

    Create PostInstallPDV.sh
    Versão 1 script de configuração PDVs

---
+++ InstalaPDV/PostInstallPDV.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+# Configura servidor DNS e adiciona o parâmetro search para que resolva o DNS do AD.
+sudo printf "nameserver 192.168.12.1\n#options edns0 trust-ad\nsearch redemachado.local" > /etc/resolv.conf
+echo "Ajustado opções no arquivo /etc/resolv.conf"
+
+# Ajusta Fuso horário
+echo "Escolha o fuso horário:"
+echo "1. Cuiabá (America/Cuiaba)"
+echo "2. São Paulo (America/Sao_Paulo)"
+
+read -p "Digite 1 ou 2: " opcao
+
+case $opcao in
+    1)
+        timedatectl set-timezone America/Cuiaba
+        hwclock -w
+        sed -i 's/UTC/LOCAL/g' /etc/adjtime
+        hwclock -w
+        sed -i 's/UTC/LOCAL/g' /etc/adjtime
+        hwclock -w
+        echo "Fuso horário definido para Cuiabá e ajustado relógio de hardware."
+        ;;
+    2)
+        timedatectl set-timezone America/Sao_Paulo
+        hwclock -w
+        sed -i 's/UTC/LOCAL/g' /etc/adjtime
+        hwclock -w
+        sed -i 's/UTC/LOCAL/g' /etc/adjtime
+        hwclock -w
+        echo "Fuso horário definido para São Paulo e ajustado relógio de hardware."
+        ;;
+    *)
+        echo "Opção inválida. Por favor, digite 1 ou 2."
+        ;;
+esac
+
+# Função para validar a hora
+validar_hora() {
+  if [[ $1 =~ ^[0-9]{2}$ ]]; then
+    if [[ $1 -ge 0 && $1 -le 23 ]]; then
+      return 0
+    fi
+  fi
+  echo "Hora inválida. Por favor, digite um número entre 0 e 23."
+  return 1
+}
+# Solicita as horas ao usuário
+read -p "Digite a hora para desligar o computador durante a semana, com dois dígitos (00-23): " hora_semana
+validar_hora "$hora_semana" || exit 1
+
+read -p "Digite a hora para desligar o computador aos domingos, com dois dígitos (00-23): " hora_domingo
+validar_hora "$hora_domingo" || exit 1
+
+# Cria as linhas para o crontab
+linha_semana="00 $hora_semana * * * /sbin/shutdown -h now"
+linha_domingo="00 $hora_domingo * * SUN /sbin/shutdown -h now"
+
+# Adiciona as linhas ao crontab
+(echo "$linha_semana"; echo "$linha_domingo") | crontab -
+sleep 5
+(echo "$linha_semana"; echo "$linha_domingo") | crontab -
+
+echo "Desligamento agendado:"
+echo "* Durante a semana: $hora_semana horas"
+echo "* Aos domingos: $hora_domingo horas"
+
+
+
+# Cópia de arquivos de interface
+echo "Etapa de clone de Interface e Clisitef, clone de um PDV do mesmo tipo"
+echo "Nunca aponte de um Self para um PDV ou de um PDV para um Self"
+echo "Aponte para um PDV do mesmo tipo que já foi configurado, cuidado!"
+read -p "Digite o IP do caixa a ser clonado (ou pressione Enter para pular essa etapa): " IP_CAIXA
+
+# Validação básica do IP
+if [[ $IP_CAIXA =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+  echo "IP válido: $IP_CAIXA"
+else
+  # IP inválido
+  echo "IP inválido ou decidiu pular a etapa. "
+  IP_CAIXA="batata"
+fi
+
+rsync -avz -I -e "ssh -p 22" root@$IP_CAIXA:/Zanthus/Zeus/Interface/ /Zanthus/Zeus/Interface/
+sleep 5
+# Nessa etapa irá copiar os arquivos de ClisiTef do mesmo PDV
+rsync -avz -I -e "ssh -p 22" root@$IP_CAIXA:/Zanthus/Zeus/pdvJava/CliSiTef.ini /Zanthus/Zeus/pdvJava/
+
+
+echo "Alterando parâmetros CUPS"
+sudo sed 's/^BrowseLocalProtocols.*$/BrowseLocalProtocols\ none/' -i /etc/cups/cupsd.conf
+cupsctl WebInterface=yes; service cups stop; service cups start
+cupsctl --remote-admin --remote-any
+printf "linux.impressora=IMP-NFE\nlinux.opcoes=3\n" > /Zanthus/Zeus/pdvJava/ZPDF00.CFG
+echo "Parâmetros CUPS ajustados com sucesso, computador será reiniciado"
+echo "Siga corretamente o manual"
+
+#Contador
+for i in {1..10}; do
+  echo "Contagem regressiva: $((10 - i))"
+  sleep 1
+done
+echo "Reiniciando"
+sudo reboot
