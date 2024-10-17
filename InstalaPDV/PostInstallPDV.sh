09530e3400b58b29c0aade977b2877f058c21a69
M4ch4d0C0l1d4r <103066475+M4ch4d0C0l1d4r@users.noreply.github.com>
2024-10-17 10:56:57 -0400

    Update PostInstallPDV.sh
    Realizados ajustes no fluxo do script, melhorias na comunicação.

--- InstalaPDV/PostInstallPDV.sh
+++ InstalaPDV/PostInstallPDV.sh
@@ -9,27 +9,29 @@ echo "Escolha o fuso horário:"
 echo "1. Cuiabá (America/Cuiaba)"
 echo "2. São Paulo (America/Sao_Paulo)"
 
-read -p "Digite 1 ou 2: " opcao
+# Laço de repetição para selecionar o correto fuso horário
+while true; do
+    read -p "Digite 1 ou 2: " opcao
+    case $opcao in
+        1)
+            timedatectl set-timezone America/Cuiaba; hwclock -w; sed -i 's/UTC/LOCAL/g' /etc/adjtime; hwclock -a
+            echo "Aguarde..."
+            sleep 5
+            timedatectl set-timezone America/Cuiaba; hwclock -w; sed -i 's/UTC/LOCAL/g' /etc/adjtime; hwclock -w
+            echo "Fuso horário definido para Cuiabá e ajustado relógio de hardware."
+            break ;;
+        2)
+            timedatectl set-timezone America/Sao_Paulo; hwclock -w; sed -i 's/UTC/LOCAL/g' /etc/adjtime; hwclock -w
+            echo "Aguarde..."
+            sleep 5
+            hwclock -w
+            timedatectl set-timezone America/Sao_Paulo; hwclock -w; sed -i 's/UTC/LOCAL/g' /etc/adjtime; hwclock -w
+            break ;;
+        *)
+            echo "Opção inválida. Por favor, digite 1 ou 2." ;;
+    esac
+done
 
-case $opcao in
-    1)
-        timedatectl set-timezone America/Cuiaba; hwclock -w; sed -i 's/UTC/LOCAL/g' /etc/adjtime; hwclock -w
-        echo "Aguarde..."
-        sleep 5
-        timedatectl set-timezone America/Cuiaba; hwclock -w; sed -i 's/UTC/LOCAL/g' /etc/adjtime; hwclock -w
-        echo "Fuso horário definido para Cuiabá e ajustado relógio de hardware."
-        ;;
-    2)
-        timedatectl set-timezone America/Sao_Paulo; hwclock -w; sed -i 's/UTC/LOCAL/g' /etc/adjtime; hwclock -w
-        echo "Aguarde..."
-        sleep 5
-        timedatectl set-timezone America/Sao_Paulo; hwclock -w; sed -i 's/UTC/LOCAL/g' /etc/adjtime; hwclock -w
-        echo "Fuso horário definido para São Paulo e ajustado relógio de hardware."
-        ;;
-    *)
-        echo "Opção inválida. Por favor, digite 1 ou 2."
-        ;;
-esac
 
 # Função para validar a hora
 validar_hora() {
@@ -41,21 +43,32 @@ validar_hora() {
   echo "Hora inválida. Por favor, digite um número entre 00 e 23. Insira com atenção!"
   return 1
 }
-# Solicita as horas ao usuário
-read -p "Digite a hora para desligar o computador durante a semana, com dois dígitos (00-23): " hora_semana
-validar_hora "$hora_semana" || exit 1
+# Solicita as horas durante a semana ao usuário
+while true; do
+  read -p "Digite a hora para desligar o computador durante a semana (00-23): " hora_semana
+  if validar_hora "$hora_semana"; then
+    break
+  fi
+done
 
-read -p "Digite a hora para desligar o computador aos domingos, com dois dígitos (00-23): " hora_domingo
-validar_hora "$hora_domingo" || exit 1
+# Solicita as horas para desligar aos domingos ao usuário
+while true; do
+  read -p "Digite a hora para desligar o computador aos domingos (00-23): " hora_domingo
+  if validar_hora "$hora_domingo"; then
+    break
+  fi
+done
 
 # Cria as linhas para o crontab
 linha_semana="00 $hora_semana * * * /sbin/shutdown -h now"
 linha_domingo="00 $hora_domingo * * SUN /sbin/shutdown -h now"
 
 # Adiciona as linhas ao crontab
+echo "Gravando linhas ao crontab, por favor aguarde"
 (echo "$linha_semana"; echo "$linha_domingo") | crontab -
 sleep 5
 (echo "$linha_semana"; echo "$linha_domingo") | crontab -
+echo "Sucesso!?"
 
 echo "Desligamento agendado:"
 echo "* Durante a semana: $hora_semana horas"
@@ -65,7 +78,6 @@ echo "* Aos domingos: $hora_domingo horas"
 echo "Etapa de clone de Interface e Clisitef, clone de um PDV do mesmo tipo"
 echo "Nunca aponte de um Self para um PDV ou de um PDV para um Self"
 echo "Aponte para um PDV do mesmo tipo que já foi configurado, cuidado!"
-echo "Responda "yes" e digite a senha do usuário, a senha será perguntada duas vezes"
 read -p "Digite o IP do caixa a ser clonado (ou pressione Enter caso tenha feito manualmente): " IP_CAIXA
 
 # Validação básica do IP
@@ -77,9 +89,12 @@ else
   IP_CAIXA="batata"
 fi
 # Comando irá clonar os arquivos de interface do PDV que apontou
+echo "O sistema pode pedir para adicionar como fonte segura, responda "yes" caso o caixa pergunte, nem sempre ele pergunta!"
+echo "Digite a senha do usuário root (consulte manual)"
 rsync -avz -I -e "ssh -p 22" root@$IP_CAIXA:/Zanthus/Zeus/Interface/ /Zanthus/Zeus/Interface/
 sleep 5
 # Nessa etapa irá copiar os arquivos de ClisiTef do PDV que apontou
+echo "Digite a senha do usuário novamente para copiar os arquivos ClisiTef"
 rsync -avz -I -e "ssh -p 22" root@$IP_CAIXA:/Zanthus/Zeus/pdvJava/CliSiTef.ini /Zanthus/Zeus/pdvJava/
 
 # Configurações do CUPS para ajustes da impressão
