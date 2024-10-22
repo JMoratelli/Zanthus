9357c2185dbed7c003cae581ecd827412447cb85
M4ch4d0C0l1d4r <103066475+M4ch4d0C0l1d4r@users.noreply.github.com>
2024-10-22 08:40:27 -0400

    Update PostInstallPDV.sh

--- InstalaPDV/PostInstallPDV.sh
+++ InstalaPDV/PostInstallPDV.sh
@@ -108,7 +108,8 @@ cupsctl WebInterface=yes; service cups stop; service cups start
 cupsctl --remote-admin --remote-any
 printf "linux.impressora=IMP-NFE\nlinux.opcoes=3\n" > /Zanthus/Zeus/pdvJava/ZPDF00.CFG
 echo "Parâmetros CUPS ajustados com sucesso, será iniciado a instalação do ScreenSaver"
-
+echo "Script feito por @jmoratelli, Jurandir Moratelli."
+sleep 5
 #Contador
 for i in {1..10}; do
   echo "Contagem regressiva: $((10 - i))"
