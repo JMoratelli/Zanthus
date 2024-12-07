d4230b484239827c3d9ed9b8b4aedd8a81d8bb1a
M4ch4d0C0l1d4r <103066475+M4ch4d0C0l1d4r@users.noreply.github.com>
2024-12-07 11:02:27 -0400

    Add files via upload

---
+++ InstalaPDV/Self/config.js
@@ -0,0 +1,7 @@
+var __modoLaboratorio = false;
+var __template = localStorage.getItem("zantplcorrente") || 2;
+var __ativaCapturaUSBProdutoSemInput = true;
+var __forcarModoTouch = true;
+var __pagamentoSCTotal = true;
+var __naoEnfileirarEventoProd = true;
+var __controleRegistroItem = true;
