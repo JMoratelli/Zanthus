a61f5c6d215572b1242ce057b49b41d1130a475d
JMoratelli <jurandirmoratelli@hotmail.com>
2025-01-13 14:35:07 -0400

    Update config.js

--- InstalaPDV/PDV/Interface/config.js
+++ InstalaPDV/PDV/Interface/config.js
@@ -2,3 +2,5 @@ var __template = localStorage.getItem("zantplcorrente") || 100;
 var __ConsultaClienteAvancada = true;
 var __colocarAtalhosNaFinalizadorasPagamento = true;
 var __exibeNumItemGridCupom = true;
+var __habilita_atalho_opcoes = true;
+var __habilita_setas_opcoes = true;
