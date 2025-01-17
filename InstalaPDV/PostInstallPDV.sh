54dbddb81f394734b610981a5d272d84ae1ef46a
JMoratelli <jurandirmoratelli@hotmail.com>
2025-01-17 17:06:28 -0400

    Update PostInstallPDV.sh

--- InstalaPDV/PostInstallPDV.sh
+++ InstalaPDV/PostInstallPDV.sh
@@ -8,9 +8,6 @@ clear
 sudo sed -i 's/#SystemMaxUse=/SystemMaxUse=1G/g; s/#RuntimeKeepFree=/SystemKeepFree=60%/g' /etc/systemd/journald.conf
 echo "Ajustando parâmetros journald.conf, aguarde..."
 sleep 5
-sudo systemctl restart systemd-journald
-echo "Reiniciando journald, aguarde..."
-sleep 5
 # Atualiza o Grub, para acelerar processo de boot.
 read -p "Deseja atualizar o Grub? (Não recomendado para dispositivos antigos) [S/n]: " resposta
 if [[ "$resposta" == "S" || "$resposta" == "s" ]]; then
