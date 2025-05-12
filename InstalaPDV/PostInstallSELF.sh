c05ac649b74582940ff80613594ff86638330042
JMoratelli <jurandirmoratelli@hotmail.com>
2025-05-12 17:35:22 -0400

    Update PostInstallSELF.sh
    ajustes na melhoria de resolução de nomes

--- InstalaPDV/PostInstallSELF.sh
+++ InstalaPDV/PostInstallSELF.sh
@@ -16,9 +16,10 @@ if [[ "$EUID" -ne 0 ]]; then
   # Se o comando su foi bem-sucedido, o script será reexecutado como root
   exit $?
 fi
-
 echo "Script sendo executado como usuário root."
 
+#Ajustes para melhoria na resposta de resolução de nomes no Linux
+sudo sed -i 's/^hosts:          files.*/hosts:          files dns/' /etc/nsswitch.conf
 #Adiciona parâmetros arquivos RESTG do MercaFacil, ajustando o TimeOut de 30 para 5.
 printf "timeout=5\n" > /Zanthus/Zeus/pdvJava/RESTG4650.CFG && printf "timeout=5\n" > /Zanthus/Zeus/pdvJava/RESTG4651.CFG
 #Remove Script Zanthus de Identificação de Balança
