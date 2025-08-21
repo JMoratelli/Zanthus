301969a0712c591e9fe87bf714e5241bf9c87231
JMoratelli <jurandirmoratelli@hotmail.com>
2025-08-21 09:55:15 -0400

    Update PostInstallPDV.sh
    Ajustado atalhos do keyd, para bloquear opções inválidas de utilização.

--- InstalaPDV/PostInstallPDV.sh
+++ InstalaPDV/PostInstallPDV.sh
@@ -31,7 +31,7 @@ git clone https://github.com/rvaiya/keyd
 cd keyd
 make && sudo make install
 sudo systemctl enable keyd && sudo systemctl start keyd
-sudo printf "[ids]\n*\n\n[main]\n\n[control]\ntab = noop\nw = noop\nt = noop\n\n[alt]\nf4 = noop\n" > /etc/keyd/default.conf
+sudo printf "[ids]\n*\n\n[main]\n\n[control]\ntab = noop\nw = noop\nt = noop\nh = noop\nb = noop\no = noop\ns = noop\nd = noop\nn = noop\n\n[alt]\nf4 = noop\n\n[control+shift]\nw = noop\nf4 = noop\n\n[meta]\nf4 = noop\n" > /etc/keyd/default.conf
 sudo keyd reload
 cd ..
 
