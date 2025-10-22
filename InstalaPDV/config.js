bbc302936f27a7229bbad3562edc036938583648
JMoratelli <jurandirmoratelli@hotmail.com>
2025-10-22 15:26:15 -0400

    Add files via upload

---
+++ InstalaPDV/config.js
@@ -0,0 +1,6 @@
+var __template = localStorage.getItem("zantplcorrente") || 100;
+var __ConsultaClienteAvancada = true;
+var __colocarAtalhosNaFinalizadorasPagamento = true;
+var __exibeNumItemGridCupom = true;
+var __habilita_atalho_opcoes = true;
+var __habilita_setas_opcoes = true;
