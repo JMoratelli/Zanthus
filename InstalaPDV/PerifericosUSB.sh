1676b83f391df14dae140a19d40ab1c99cbb10b0
JMoratelli <jurandirmoratelli@hotmail.com>
2026-08-01 10:36:28 -0400

    Implement USB reader detection for Honeywell first
    Added support for Honeywell USB reader before Datalogic reader.

--- InstalaPDV/PerifericosUSB.sh
+++ InstalaPDV/PerifericosUSB.sh
@@ -17,16 +17,24 @@ ln -sf /dev/ttyACM$Y /dev/ttyS4 2>/dev/null
 # ==========================================
 if [ -f "$arquivo_conf_self" ]; then
     
-    # Busca pela string do leitor Datalogic
-    output_leitor=$(ls -l /dev/serial/by-id/* 2>/dev/null | grep 'usb-Datalogic')
-    Z=$(echo "$output_leitor" | grep -oP 'ttyACM\K\d')
+    # 1. Busca primeiro pela string do leitor Honeywell
+    output_leitor=$(ls -l /dev/serial/by-id/* 2>/dev/null | grep 'usb-Honeywell')
     
+    # 2. Se a variável estiver vazia (não achou Honeywell), tenta o Datalogic
+    if [ -z "$output_leitor" ]; then
+        output_leitor=$(ls -l /dev/serial/by-id/* 2>/dev/null | grep 'usb-Datalogic')
+    fi
+    
+    # 3. Extrai o número da porta ttyACM 
+    # (Usei \d+ no final para garantir que funcione se a porta for ttyACM10 ou maior)
+    Z=$(echo "$output_leitor" | grep -oP 'ttyACM\K\d+')
+    
+    # 4. Se encontrou alguma porta (Z não é vazio), faz a substituição
     if [ -n "$Z" ]; then
         mv -f /dev/ttyS0 /dev/ttyS100 2>/dev/null
         ln -sf /dev/ttyACM$Z /dev/ttyS0 2>/dev/null
     fi
 fi
-
 # ==========================================
 # 3. AUTOMAÇÃO DO SINALEIRO
 # ==========================================
